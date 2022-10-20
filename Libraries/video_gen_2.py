30431f09dd12cb1c74da4005854d4ecc7e673b08
dv1sual <53606943+dv1sual@users.noreply.github.com>
2022-10-20 22:36:43 +0100

    video_gen_2 work

--- Libraries/video_gen_2.py
+++ Libraries/video_gen_2.py
@@ -1,8 +1,11 @@
 import ffmpeg
+import os
 
+image_folder = 'D:\\d3 Projects\\diag_start_Trinity_2022\\d3 Projects\\start\\objects\\VideoFile\\test patterns'
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 (
     ffmpeg
-    .input('D:\\d3 Projects\\diag_start_Trinity_2022\\d3 Projects\\start\\objects\\VideoFile\\test patterns\D3_test_16-9.png', framerate=25)
+    .input(image_folder, framerate=25)
     .filter('deflicker', mode='pm', size=10)
     .filter('scale', size='3840:2160', force_original_aspect_ratio='increase')
     .output('movie.mov', vcodec='hap', crf=20, preset='slower', movflags='faststart', pix_fmt='rgba')
